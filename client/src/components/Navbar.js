4c7d899cd765e25ce965b6fc7e09e508027f2e78
NCMoseley <ncmoseley@gmail.com>
2018-11-02 13:10:24 -0700

    Add signout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import Signout from './auth/Signout';
 
 export const Navbar = ({ session }) => (
   <nav>
-    {session && session.getCurrentUser ? <NavbarAuth /> : <NavbarUnAuth />}
+    {session && session.getCurrentUser ? (
+      <NavbarAuth session={session} />
+    ) : (
+      <NavbarUnAuth />
+    )}
   </nav>
 );
 
-const NavbarAuth = () => (
-  <ul>
-    <li>
-      <NavLink to="/" exact>
-        Home
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/search">Search</NavLink>
-    </li>
-    <li>
-      <NavLink to="/project/add">Add project</NavLink>
-    </li>
-    <li>
-      <NavLink to="/profile">Profile</NavLink>
-    </li>
-    <li>
-      <button>Signout</button>
-    </li>
-  </ul>
+const NavbarAuth = ({ session }) => (
+  <Fragment>
+    <ul>
+      <li>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/search">Search</NavLink>
+      </li>
+      <li>
+        <NavLink to="/project/add">Add project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Profile</NavLink>
+      </li>
+      <li>
+        <Signout />
+      </li>
+    </ul>
+    <h4>
+      Welcome, <strong>{session.getCurrentUser.username}</strong>
+    </h4>
+  </Fragment>
 );
 
 const NavbarUnAuth = () => (
